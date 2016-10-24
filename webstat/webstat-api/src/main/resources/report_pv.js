ded8b6720f298b909659a2d40d0166a62cb765ac
78969468 <78969468@qq.com>
2016-10-24 15:54:08 +0800

    修改，可手工控制是否上报page view

--- webstat/webstat-api/src/main/resources/report_pv.js
+++ webstat/webstat-api/src/main/resources/report_pv.js
@@ -1,24 +1,3 @@
-var _referrer = encodeURIComponent(document.referrer);
-var _campaign = _getParameterByName('_campaign');
-var _adTag = _getParameterByName('_adTag');
-var _pageName = document.title;
-var logPvSrc = "${host}/webstat/api/logPv?appId=${appId}"
-	+ "&_pageName=" + _pageName
-    + ((_referrer == "" || _referrer == null) ? "" : "&_referer=" + _referrer)
-    + ((_campaign == "" || _campaign == null) ? "" : "&_campaign=" + _campaign)   
-    + ((_adTag == "" || _adTag == null) ? "" : "&_adTag=" + _adTag);
-
-function _getParameterByName(name) {
-	name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-	var regexS = "[\\?&]" + name + "=([^&#]*)";
-	var regex = new RegExp(regexS);
-	var results = regex.exec(window.location.search);
-	if (results == null)
-		return "";
-	else
-		return decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-
 if (document.head) {
     loadScript(logPvSrc);
 }
